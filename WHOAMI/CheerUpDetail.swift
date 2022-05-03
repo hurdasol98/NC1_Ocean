5dc73b890629cda902e66f34406395b9494bcba1
hurdasol98 <73656470+hurdasol98@users.noreply.github.com>
2022-05-03 12:54:37 +0900

    CheerUpDetail뷰에서 몇 번째 사진인지 표시 opacity조절 구현

--- WHOAMI/CheerUpDetail.swift
+++ WHOAMI/CheerUpDetail.swift
@@ -4,14 +4,15 @@
 //
 //  Created by hurdasol on 2022/05/01.
 //
-// 세 번째 탭 뷰의 서브뷰, 확대된 이미지를 볼 수 있다.d
-
+// 세 번째 탭 뷰의 서브뷰, 확대된 이미지를 볼 수 있다.d;
 import SwiftUI
-
 struct CheerUpDetail: View {
     
     @State var id: Int
+    //몇 번재 사진인지 표시하는 text opacity 조절
+    @State private var textSwitch = true
     var body: some View {
+        
         //탭뷰 페이지모드 구현
         TabView(selection: $id){
             ForEach(imageSet){img in
@@ -19,10 +20,24 @@ struct CheerUpDetail: View {
                     HStack{
                         Spacer()
                         Text("\(String(img.id+1) + " / " + String(imageSet.count))")
+                            .padding(.horizontal,5)
                             .foregroundColor(.white)
                             .background(RoundedRectangle(cornerRadius: 5))
                             .foregroundColor(.gray)
-                          
+                            .opacity(textSwitch ? 1 : 0.1)
+                            .animation(.easeInOut(duration: 0.2))
+                            .onAppear(perform: {
+                                DispatchQueue.main.asyncAfter(deadline: .now() + 0.1){
+                                    if self.textSwitch == false{
+                                        self.textSwitch.toggle()
+                                    }
+                                    DispatchQueue.main.asyncAfter(deadline: .now() + 0.5){
+                                        self.textSwitch.toggle()
+                                    }
+                                    
+                                }
+                            })
+                            
                     }.offset(y:40)
                         .zIndex(1)
                     Image(img.imageName)
@@ -32,16 +47,16 @@ struct CheerUpDetail: View {
                         .navigationTitle(img.imageText)
                 }
             }
-
+            
         }.tabViewStyle(PageTabViewStyle(indexDisplayMode: .automatic))
-                        
-
-
-
-
-
-
-
+        
+        
+        
+        
+        
+        
+        
+        
     }
 }
 
